63be15785bb98a8f92f4acca8f3c4dd98a5a8dac
pritam dutta <pritamdutta7498@gmail.com>
2024-03-15 13:26:00 +0600

    showing the winner in display

--- script.js
+++ script.js
@@ -6,27 +6,27 @@ let turn0 = true;
 
 // pattern  for the game (X and O)
 const winPattern = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [0,4,8],
-    [2,4,6],
-    [1,4,7],
-    [2,5,8],
-  
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [0, 4, 8],
+    [2, 4, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
 ];
 
 
 // select each box using forEach and addEventListener
 
-boxes.forEach((box)=>{
-    box.addEventListener('click', ()=>{
+boxes.forEach((box) => {
+    box.addEventListener('click', () => {
         console.log('box was clicked');
-        if(turn0){
+        if (turn0) {
             box.innerText = '0';
             turn0 = false;
-        }else{
+        } else {
             box.innerText = 'X';
             turn0 = true;
         }
@@ -37,13 +37,26 @@ boxes.forEach((box)=>{
 })
 
 // winner checking function
-const checkWinner = () =>{
-    for(let pattern of winPattern){
-        console.log(boxes[pattern[0]].innerText,
-            boxes[pattern[1]].innerText,
-            boxes[pattern[2]].innerText,
-            )
-             
-            
+const checkWinner = () => {
+    for (let pattern of winPattern) {
+        // console.log(boxes[pattern[0]].innerText,
+        //     boxes[pattern[1]].innerText,
+        //     boxes[pattern[2]].innerText,
+        //     )
+
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        // postion value checking for winner
+        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log('Winner' , pos1Val)
+             document.getElementById('winner').innerText = `Congratulation Winner is: ${pos1Val}`;
+            }
+        }
+
     }
 }
+
+// reset game function
